3b997b009ff44a98cc35b7e4a861a15842135358
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2025-07-30 15:15:59 -0300

    Update index2.php

--- www/public_html/sys/admin/index2.php
+++ www/public_html/sys/admin/index2.php
@@ -159,7 +159,7 @@ if($Enviar) {
                      }
         } else { 
                 gravaLog_LoginSys("Login Error (2 Invalido): $login_var", true);
-                header("Location: index.php?Invalido=$passw");
+                header("Location: index.php?Invalido=1");
                 exit;
         }
         pg_close($connid); 
