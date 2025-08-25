9354520c96983a27aba0d4bb3580fc023389c354
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2025-08-25 14:44:15 -0300

    Add migration autenticador backoffice

---
+++ www/migration/migrations/006_migration_autenticador_bo.php
@@ -0,0 +1,16 @@
+<?php
+
+return function (PDO $pdo) {
+    $pdo->exec("ALTER TABLE usuarios
+                    ADD COLUMN chave_autenticador TEXT NOT NULL DEFAULT '',
+                    ADD COLUMN sem_aut_data DATE NOT NULL DEFAULT CURRENT_DATE;");
+                    
+    $pdo->exec("CREATE TABLE usuarios_bo_dispositivos (
+                	id serial4 NOT NULL,
+                	user_id int4 NOT NULL,
+                	device_token varchar(100) NOT NULL,
+                	expires_at timestamp DEFAULT (now() + '30 days'::interval) NULL,
+                	created_at timestamp DEFAULT now() NULL,
+                	CONSTRAINT usuarios_bo_dispositivos_pkey PRIMARY KEY (id)
+                );");
+};
