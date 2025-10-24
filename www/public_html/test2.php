415f44723388f22f0cd0898f573669c623c8c7d7
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2025-10-24 11:22:39 -0300

    Update test2.php

--- www/public_html/test2.php
+++ www/public_html/test2.php
@@ -10,8 +10,8 @@ $efinanceira = new GerarEFinanceira();
 
 //$xmlAbertura = $efinanceira->gerarAbertura('2025-09-01', '2025-09-30');
 //$xmlAbertura = $efinanceira->gerarFechamento('2025-09-01', '2025-09-30');
-$xmlAbertura = $efinanceira->gerarMovimentacaoFinanceira(1, '12345678901', 'Joao Mota', '2000-02-10', 'Rua teste da silve', '2025', '09', 1111, 999.99, 200.11);
-// $xmlAbertura = $efinanceira->gerarCadastroDeclarante();
+//$xmlAbertura = $efinanceira->gerarMovimentacaoFinanceira(1, '12345678901', 'Joao Mota', '2000-02-10', 'Rua teste da silve', '2025', '09', 1111, 999.99, 200.11);
+ $xmlAbertura = $efinanceira->gerarCadastroDeclarante();
 
 header('Content-Type: application/xml; charset=utf-8');
 echo $xmlAbertura->saveXML();
