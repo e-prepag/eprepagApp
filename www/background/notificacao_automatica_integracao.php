a27419d7a39b8911466511a140bc5ec50c764b27
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2025-11-28 17:07:56 -0300

    Diretiva para notificar email também com pedido passou de 30 min conciliado

--- www/background/notificacao_automatica_integracao.php
+++ www/background/notificacao_automatica_integracao.php
@@ -41,6 +41,7 @@ if (!$arquivoLog->haveFile()) {
             vg.vg_ultimo_status, 
             vg.vg_pagto_tipo,
             vg.vg_data_concilia,
+            (NOW() - vg_data_concilia) > INTERVAL '30 minutes' AS passou_30_min,
             coalesce(vg_id, 0) as vg_id
     from tb_integracao_pedido ip 
         left outer join tb_venda_games vg on ip.ip_vg_id = vg.vg_id 
@@ -97,7 +98,7 @@ if (!$arquivoLog->haveFile()) {
 
             echo "$sret $post_parameters<br>";
 
-            if ($buffer == false || $http_code != 200) {
+            if ($buffer == false || $http_code != 200 || $rs_row['passou_30_min']) {
                 $msg .= " Pedido " . $rs_row['vg_id'] . " => Conciliado em [" . $rs_row['vg_data_concilia'] . "] => Tipo de pagamento [" . $rs_row['vg_pagto_tipo'] . "]; <br>" . PHP_EOL;
             }
         } //end while
