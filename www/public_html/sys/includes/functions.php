ca659158bdee0aba07c957b36732d664b83be8cd
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2025-12-05 16:16:05 -0300

    Update functions.php

--- www/public_html/sys/includes/functions.php
+++ www/public_html/sys/includes/functions.php
@@ -140,6 +140,17 @@ if (!function_exists("SQLexecuteQuery")) {
 
 		return $ret;
 	}
+	function SQLexecuteQueryParams($sql, $params)
+{
+	$ret = pg_query_params($GLOBALS['connid'], $sql, $params);
+	if (strlen($erro = pg_last_error($GLOBALS['connid']))) {
+		$message  = date("Y-m-d H:i:s") . " ";
+		$message .= "Erro: " . $erro . "<br>\n";
+		$message .= "Query: " . $sql . "<br>\n";
+		gravaLog_SQLexecuteQuery($message);
+	}
+	return $ret;
+}
 }
 
 function getValue($sql)
