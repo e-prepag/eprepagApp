cf3b6270392ba16da744973e91d9745f31a869e5
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-01-09 16:59:53 -0300

    Update nova.php
    Valida sem precisar verificar se está ativa

--- www/public_html/game/conta/nova.php
+++ www/public_html/game/conta/nova.php
@@ -198,7 +198,7 @@ if (isset($_POST['login']) && !empty($_POST['login'])) {
                     }
                 } else {
                     $cpfFinal = substr($cpf, 0, 3) . "." . substr($cpf, 3, 3) . "." . substr($cpf, 6, 3) . "-" . substr($cpf, 9, 2);
-                    $sql = "select * from usuarios_games where ug_cpf = $1 and ug_ativo = 1;";
+                    $sql = "select * from usuarios_games where ug_cpf = $1;";
                     $buscaUsuario = SQLexecuteQueryParams($sql, [$cpfFinal]);
 
                     $linhas = pg_num_rows($buscaUsuario);
