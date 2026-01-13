1ff3440153074b81717e3b2b261ea8f62003021d
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-01-13 11:38:13 -0300

    Update busca_cpf_venda_pdv.php

--- www/backoffice/pdv/busca_cpf_venda_pdv.php
+++ www/backoffice/pdv/busca_cpf_venda_pdv.php
@@ -11,18 +11,13 @@ require_once $raiz_do_projeto."class/util/Util.class.php";
 require_once $raiz_do_projeto."includes/gamer/constantes.php";
 require_once $raiz_do_projeto."includes/complice/functions.php";
 
-$dataClickIni = $_POST['dataClickIni'] ?? null;
-$dataClickFim = $_POST['dataClickFim'] ?? null;
-$publishers = $_POST['publishers'] ?? null;
-$cpf = $_POST['cpf'] ?? null;
-$busca = $_POST['busca'] ?? null;
-$buscar = $_POST['buscar'] ?? null;
-$dataClickIni = $_GET['dataClickIni'] ?? null;
-$dataClickFim = $_GET['dataClickFim'] ?? null;
-$publishers = $_GET['publishers'] ?? null;
-$cpf = $_GET['cpf'] ?? null;
-$busca = $_GET['busca'] ?? null;
-$buscar = $_GET['buscar'] ?? null;
+$dataClickIni = $_REQUEST['dataClickIni'] ?? null;
+$dataClickFim = $_REQUEST['dataClickFim'] ?? null;
+$publishers = $_REQUEST['publishers'] ?? null;
+$cpf = $_REQUEST['cpf'] ?? null;
+$busca = $_REQUEST['busca'] ?? null;
+$buscar = $_REQUEST['buscar'] ?? null;
+
 
 if(empty($dataClickIni) || !Util::checkValidDate($dataClickIni)) 
     $dataClickIni = date('d/m/Y');
