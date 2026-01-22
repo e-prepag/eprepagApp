fd043108c868bf9405452b48b2129d6f60061bd2
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-01-22 15:20:40 -0300

    Remiver algumas funções que não serão utilizadas

--- www/backoffice/e_financeira/gerar_zip.php
+++ www/backoffice/e_financeira/gerar_zip.php
@@ -5,56 +5,12 @@ $data_inicial = isset($_GET['data_inicial']) ? urldecode($_GET['data_inicial']) 
 $data_final = isset($_GET['data_final']) ? urldecode($_GET['data_final']) : date('Y-m');
 
 if ($_GET['acao'] == "baixar") {
-    $lotes = gerarXmlMovimentacao($data_inicial, $data_final);
+    
+	$efinanceira = new GerarEFinanceira();
+    $lotes = $efinanceira->gerarXmlMovimentacao($data_inicial, $data_final);
 
     if ($lotes) {
         $caminho = gerarZipLotes($lotes, "lotes_{$data_final}_{$data_inicial}_" . date('Ymd_Hi') . ".zip");
-
-        // Validação básica
-        if (!$caminho || !file_exists($caminho)) {
-            http_response_code(404);
-            exit('Arquivo não encontrado');
-        }
-
-        header('Content-Type: application/zip');
-        header('Content-Disposition: attachment; filename="' . $arquivo . '"');
-        header('Content-Length: ' . filesize($caminho));
-        header('Cache-Control: no-cache');
-
-        readfile($caminho);
-        unlink($caminho);
-    }else{
-        echo "Erro ao gerar o arquivo";
-    }
-}
-if ($_GET['acao'] == "criptografar") {
-    $lotes = gerarXmlMovimentacao($data_inicial, $data_final);
-
-    if ($lotes) {
-        $caminho = gerarZipCripto($lotes, "lotes_{$data_final}_{$data_inicial}_" . date('Ymd_Hi') . ".zip");
-
-        // Validação básica
-        if (!$caminho || !file_exists($caminho)) {
-            http_response_code(404);
-            exit('Arquivo não encontrado');
-        }
-
-        header('Content-Type: application/zip');
-        header('Content-Disposition: attachment; filename="' . $arquivo . '"');
-        header('Content-Length: ' . filesize($caminho));
-        header('Cache-Control: no-cache');
-
-        readfile($caminho);
-        unlink($caminho);
-    }else{
-        echo "Erro ao gerar o arquivo";
-    }
-}
-if ($_GET['acao'] == "enviar") {
-    $lotes = gerarXmlMovimentacao($data_inicial, $data_final);
-
-    if ($lotes) {
-        $caminho = enviarLotesEfinanceira($lotes, "lotes_{$data_final}_{$data_inicial}_" . date('Ymd_Hi') . ".zip");
 
         // Validação básica
         if (!$caminho || !file_exists($caminho)) {
