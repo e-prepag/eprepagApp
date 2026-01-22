81e117ec43ab1f1022f8bbb05309d762d64c73c4
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-01-22 15:18:45 -0300

    adiciona os indices para a consulta ficar mais rápida

--- www/migration/migrations/019_ajustes_efinanceira.php
+++ www/migration/migrations/019_ajustes_efinanceira.php
@@ -6,5 +6,17 @@ return function (PDO $pdo) {
                     ADD COLUMN cpfcnpj_declarado text NULL,
                     ADD COLUMN data_anomes text NULL,
                     ADD COLUMN id_retificacao bigint NULL,
-                    ADD COLUMN num_protocolo text NULL;");
+                    ADD COLUMN num_protocolo text NULL;
+                    
+                    CREATE INDEX idx_envios_data_anomes 
+                        ON public.envios_e_financeira (data_anomes);
+
+                    CREATE INDEX idx_envios_cpfcnpj 
+                        ON public.envios_e_financeira (cpfcnpj_declarado);
+
+                    CREATE INDEX idx_envios_protocolo 
+                        ON public.envios_e_financeira (num_protocolo);
+
+                    CREATE INDEX idx_envios_busca_combinada 
+                        ON public.envios_e_financeira (cpfcnpj_declarado, data_anomes);");
 };
