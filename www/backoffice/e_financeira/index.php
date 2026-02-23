65a759af55d1d36c16f1f9db842f6d555691a4f1
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-02-23 12:08:40 -0300

    Atualiza remoção de header de teste

--- www/backoffice/e_financeira/index.php
+++ www/backoffice/e_financeira/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '/www/includes/constantes.php';
-require_once $raiz_do_projeto . "backoffice/includes/topo_teste.php";
+require_once $raiz_do_projeto . "backoffice/includes/topo.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
