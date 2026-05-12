4e8e8c154c2775202dfe6250fefeb1e98725a131
JoseCarlosEasyCode <jose.carlos@easygroupit.com>
2026-05-12 15:18:01 -0300

    Update cadastra_estorno.php

--- www/backoffice/gamer/cadastra_estorno.php
+++ www/backoffice/gamer/cadastra_estorno.php
@@ -8,10 +8,31 @@ require_once $raiz_do_projeto."class/classDescriptionReport.php";
 $descricao = new DescriptionReport('estorno');
 echo $descricao->MontaAreaDescricao();
 
+$btn_estorno = $_POST['btn_estorno'] ?? null;
+$btn_pesquisar = $_POST['btn_pesquisar'] ?? null;
+$scf_id = $_POST['scf_id'] ?? array();
+$motivo = trim((string)($_POST['motivo'] ?? ''));
+$ug_id = trim((string)($_POST['ug_id'] ?? ''));
+$vg_id = trim((string)($_POST['vg_id'] ?? ''));
+$mensagem_final = "";
+
+function cadastra_estorno_h($value) {
+	return htmlspecialchars((string)$value, ENT_QUOTES, 'ISO-8859-1');
+}
+
 if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 	//echo "<pre>".print_r($scf_id,true)."</pre>";
-	$mensagem_final = "";
-	$scf_id_lista = !empty($scf_id) ? array_values((array)$scf_id) : array(0);
+	$scf_id_lista = array();
+	foreach ((array)$scf_id as $scf_id_param) {
+		if (ctype_digit((string)$scf_id_param)) {
+			$scf_id_lista[] = (int)$scf_id_param;
+		}
+	}
+
+	if (empty($scf_id_lista)) {
+		$scf_id_lista = array(0);
+	}
+
 	$scf_id_placeholders = array();
 	foreach ($scf_id_lista as $scf_id_index => $scf_id_param) {
 		$scf_id_placeholders[] = "$" . ($scf_id_index + 1);
@@ -47,15 +68,15 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 //					echo $sql."<br>";
 					if($rs_usuarios_games) {
 						$sql = "INSERT INTO tb_pag_estorno (ug_id,tpe_data,tpe_valor,tpe_motivo,tpe_id_estonador,tpe_tipo_user) VALUES ((select ug_id from saldo_composicao_fifo where scf_id=$1),NOW(),(select scf_valor from saldo_composicao_fifo where scf_id=$1),$2,$3,'G');";
-						$rs_estorno = SQLexecuteQueryParams($sql, array($rs_dados_confirmados_row['scf_id'], $motivo, $_SESSION['iduser_bko']));
+							$rs_estorno = SQLexecuteQueryParams($sql, array($rs_dados_confirmados_row['scf_id'], $motivo, $_SESSION['iduser_bko']));
 //						$rs_estorno = true;
 //						echo $sql."<br>";
 						if($rs_estorno) {
 							$sql = "SELECT * FROM usuarios_games ug INNER JOIN saldo_composicao_fifo scf ON (ug.ug_id=scf.ug_id) WHERE scf_id=$1;";
 							$rs_dados_sucesso = SQLexecuteQueryParams($sql, array($rs_dados_confirmados_row['scf_id']));
 //							echo $sql."<br>";
-							if($rs_dados_sucesso_row = pg_fetch_array($rs_dados_sucesso)) {
-								$mensagem_final .= "<font color='#FF0000'><b>Estorno do Gamer [".$rs_dados_sucesso_row['ug_nome']."] foi realizado com sucesso, seu saldo atual é R$ ".number_format($rs_dados_sucesso_row['ug_perfil_saldo'], 2, ',', '.')." para a venda ID [".$rs_dados_sucesso_row['vg_id']."] foi estornado o valor de R$ ".number_format($rs_dados_sucesso_row['scf_valor'], 2, ',', '.').".\n</b></font><br>";
+								if($rs_dados_sucesso_row = pg_fetch_array($rs_dados_sucesso)) {
+									$mensagem_final .= "<font color='#FF0000'><b>Estorno do Gamer [".cadastra_estorno_h($rs_dados_sucesso_row['ug_nome'])."] foi realizado com sucesso, seu saldo atual é R$ ".number_format($rs_dados_sucesso_row['ug_perfil_saldo'], 2, ',', '.')." para a venda ID [".cadastra_estorno_h($rs_dados_sucesso_row['vg_id'])."] foi estornado o valor de R$ ".number_format($rs_dados_sucesso_row['scf_valor'], 2, ',', '.').".\n</b></font><br>";
 							}//end if($rs_dados_sucesso)
 							else {
 								$msg .= "<font color='#FF0000'><b>Erro ao selecionar dados de confirmaçãode sucesso da operação(".$rs_dados_confirmados_row['scf_id'].").\n</b></font><br>";
@@ -78,7 +99,7 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 			}//end else if($rs_saldo_composicao_fifo)
 		} // end if($msg == "")
 	}//end while($rs_dados_confirmados && ($rs_dados_confirmados_row = pg_fetch_array($rs_dados_confirmados)))
-	
+
 	//Finaliza transacao
 	if($msg == ""){
 		$sql = "COMMIT TRANSACTION ";
@@ -96,7 +117,7 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 	<link rel="stylesheet" type="text/css" href="/css/cssClassLista.css" />
 	<script>
 	function validaCheckbox(formObj, chkName){
-		var chkObj = formObj[chkName]; 
+		var chkObj = formObj[chkName];
 		if(!chkObj) return false;
 		var msg = "";
 
@@ -118,11 +139,11 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 
 	function VerificaMotivo() {
 		if(validaCheckbox(document.form1,'scf_id[]')) {
-			if(document.form1.motivo.value=="") { 
-				alert("Você tem informar o motivo do estorno!"); 
-				return false; 
+			if(document.form1.motivo.value=="") {
+				alert("Você tem informar o motivo do estorno!");
+				return false;
 			} //end if(document.form1.motivo.value=="")
-			else { 
+			else {
 				if (confirm("Você tem certeza que deseja realizar\no estorno para este depósito?"))
 					return true;
 				else return false;
@@ -132,9 +153,9 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 			return false;
 		}//end else if(validaCheckbox(document.form1,'scf_id[]'))
 	}//end function VerificaMotivo()
-        
+
         $(function(){
-        
+
             $("#checkall").click(function(){
 
                 var res = this.checked;
@@ -154,7 +175,7 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
         <li class="active"><a href="<?php echo $sistema->item->getLink() ; ?>"><?php echo $sistema->item->getDescricao() ; ?></a></li>
     </ol>
 </div>
-<form id="form1" name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form id="form1" name="form1" method="post" action="<?php echo cadastra_estorno_h($_SERVER['PHP_SELF'] ?? ''); ?>">
 <table class="table txt-preto fontsize-pp">
     <tr>
         <td valign="top">
@@ -168,11 +189,11 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
                                 <tr>
                                     <td align="right">ID do Usu&aacute;rio: </td>
                                     <td>
-										<input name="ug_id" type="text" id="ug_id" size="20" maxlength="10" value="<?php if(isset($ug_id)) echo $ug_id;?>"/>
+											<input name="ug_id" type="text" id="ug_id" size="20" maxlength="10" value="<?php echo cadastra_estorno_h($ug_id); ?>"/>
 									</td>
                                     <td align="right">ID da Venda: </td>
                                     <td>
-										<input name="vg_id" type="text" id="vg_id" size="20" maxlength="10" value="<?php if(isset($vg_id)) echo $vg_id;?>"/>
+											<input name="vg_id" type="text" id="vg_id" size="20" maxlength="10" value="<?php echo cadastra_estorno_h($vg_id); ?>"/>
 									</td>
                                 </tr>
                                 <tr>
@@ -188,23 +209,27 @@ if (isset($btn_estorno) && $btn_estorno=="Estornar") {
 if (isset($btn_pesquisar) && $btn_pesquisar=="Pesquisar") {
 
 
-$sql = "SELECT scf.*,to_char(scf_data_deposito,'DD/MM/YYYY HH:MI:SS') as data_deposito, ug_nome
-	 FROM saldo_composicao_fifo scf 
+	$sql = "SELECT scf.*,to_char(scf_data_deposito,'DD/MM/YYYY HH:MI:SS') as data_deposito, ug_nome
+	 FROM saldo_composicao_fifo scf
 		INNER JOIN tb_venda_games vg ON (scf.vg_id=vg.vg_id)
 		INNER JOIN usuarios_games ug ON (scf.ug_id=ug.ug_id) ";
-if (!empty($ug_id))
-	$sql_aux[] = "scf.ug_id = ". $ug_id . " ";
-if (!empty($vg_id))
-	$sql_aux[] = "scf.vg_id = ". $vg_id . " ";
-if (isset($sql_aux) && is_array($sql_aux)) {
-	$sql .= ' WHERE ' . implode(' AND ', $sql_aux) . ' AND scf_valor=scf_valor_disponivel AND scf_status=1';
-}
-$sql .= ' GROUP BY scf.scf_id,scf.ug_id,scf_data_deposito,scf_valor,scf_valor_disponivel,scf_status,scf_canal,scf_comissao,scf_id_pagamento,scf.vg_id,data_deposito,ug_nome
-		  ORDER BY scf_data_deposito DESC';
-//echo str_replace("\n", "<br>\n", $sql)."<br>";
-//die($sql);
-$rsResposta = SQLexecuteQuery($sql);
-}//end if ($btn_pesquisar=="Pesquisar")
+	$sql_aux = array("scf_valor=scf_valor_disponivel", "scf_status=1");
+	$sql_params = array();
+	if ($ug_id !== "" && ctype_digit($ug_id)) {
+		$sql_params[] = (int)$ug_id;
+		$sql_aux[] = "scf.ug_id = $" . count($sql_params);
+	}
+	if ($vg_id !== "" && ctype_digit($vg_id)) {
+		$sql_params[] = (int)$vg_id;
+		$sql_aux[] = "scf.vg_id = $" . count($sql_params);
+	}
+	$sql .= ' WHERE ' . implode(' AND ', $sql_aux);
+	$sql .= ' GROUP BY scf.scf_id,scf.ug_id,scf_data_deposito,scf_valor,scf_valor_disponivel,scf_status,scf_canal,scf_comissao,scf_id_pagamento,scf.vg_id,data_deposito,ug_nome
+			  ORDER BY scf_data_deposito DESC';
+	//echo str_replace("\n", "<br>\n", $sql)."<br>";
+	//die($sql);
+	$rsResposta = count($sql_params) ? SQLexecuteQueryParams($sql, $sql_params) : SQLexecuteQuery($sql);
+	}//end if ($btn_pesquisar=="Pesquisar")
 ?>
 <table class="table txt-preto fontsize-pp">
 <?php
@@ -235,12 +260,12 @@ if(isset($rsResposta) && $rsResposta){
     while ($pgResposta = pg_fetch_array ($rsResposta)) {
 ?>
         <tr<?php echo " bgcolor='".$bck."'" ?>>
-            <td align="center"><input name="scf_id[]" type="checkbox" id="scf_id[]" value="<?php echo $pgResposta['scf_id']; ?>"/></td>
-            <td align="left"><nobr><a href="/gamer/usuarios/com_usuario_detalhe.php?usuario_id=<?php echo $pgResposta['ug_id'];?>"><?php echo $pgResposta['ug_nome'];?></a></nobr></td>
-            <td align="center"><nobr><?php echo $pgResposta['data_deposito'];?></nobr></td>
+            <td align="center"><input name="scf_id[]" type="checkbox" id="scf_id[]" value="<?php echo (int)$pgResposta['scf_id']; ?>"/></td>
+            <td align="left"><nobr><a href="/gamer/usuarios/com_usuario_detalhe.php?usuario_id=<?php echo (int)$pgResposta['ug_id'];?>"><?php echo cadastra_estorno_h($pgResposta['ug_nome']);?></a></nobr></td>
+            <td align="center"><nobr><?php echo cadastra_estorno_h($pgResposta['data_deposito']);?></nobr></td>
             <td align="right"><nobr><?php echo number_format($pgResposta['scf_valor'], 2, ',', '.');?></nobr></td>
-            <td align="center"><nobr><?php echo $FORMAS_PAGAMENTO_DESCRICAO[$pgResposta['scf_id_pagamento']];?></nobr></td>
-            <td align="center"><nobr><a href="/gamer/vendas/com_venda_detalhe.php?venda_id=<?php echo $pgResposta['vg_id'];?>"><?php echo $pgResposta['vg_id'];?></a></nobr></td>
+            <td align="center"><nobr><?php echo cadastra_estorno_h($FORMAS_PAGAMENTO_DESCRICAO[$pgResposta['scf_id_pagamento']] ?? '');?></nobr></td>
+            <td align="center"><nobr><a href="/gamer/vendas/com_venda_detalhe.php?venda_id=<?php echo (int)$pgResposta['vg_id'];?>"><?php echo (int)$pgResposta['vg_id'];?></a></nobr></td>
         </tr>
 <?php
         if ($bck == $backcolor1)
@@ -253,7 +278,7 @@ if(isset($rsResposta) && $rsResposta && ((($rsResposta) ? pg_num_rows($rsRespost
 	<tr>
         <td align="center">&nbsp;</td>
         <td align="center" colspan="4">
-			Motivo do Estorno: <input name="motivo" type="text" id="motivo" size="80" maxlength="256" value="<?php if(isset($motivo)) echo $motivo;?>"/><br>
+				Motivo do Estorno: <input name="motivo" type="text" id="motivo" size="80" maxlength="256" value="<?php echo cadastra_estorno_h($motivo); ?>"/><br>
 			<input name="btn_estorno" type="submit" id="btn_estorno" value="Estornar" OnClick='javascript: return VerificaMotivo();'/>
 		</td>
         <td align="center">&nbsp;</td>
